ebb9cbf20076836fde71c43fbc9845b78f7fd108
Ivan Chetverkin <104260618+IvanChet-4@users.noreply.github.com>
2025-02-07 00:14:31 +0300

    Update outputs.tf

--- HW2-2/outputs.tf
+++ HW2-2/outputs.tf
@@ -1,15 +1,12 @@
 output "instances_info" {
-  value = [
-    {
-      instance_name = yandex_compute_instance.web.name
-      external_ip   = yandex_compute_instance.web.network_interface[0].ipv4_address
-      fqdn          = "${yandex_compute_instance.web.name}.${var.dns_zone}"
-    },
-    {
-      instance_name = yandex_compute_instance.db.name
-      external_ip   = yandex_compute_instance.db.network_interface[0].ipv4_address
-      fqdn          = "${yandex_compute_instance.db.name}.${var.dns_zone}"
+  value = {
+    platform = {
+      instance_name = yandex_compute_instance.platform.name
+      external_ip   = yandex_compute_instance.platform.network_interface[0].nat_ip_address
     }
-  ]
-   description = "Instances info"
+    db = {
+      instance_name = yandex_compute_instance.db.name
+      external_ip   = yandex_compute_instance.db.network_interface[0].nat_ip_address
+    }
+  }
 }
